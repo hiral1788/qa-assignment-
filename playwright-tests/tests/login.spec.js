3aabff977c0a0accf43c3a59c655b97e66507078
hiral1788 <140881144+hiral1788@users.noreply.github.com>
2026-01-06 22:54:51 +0530

    Update login.spec.js

--- playwright-tests/tests/login.spec.js
+++ playwright-tests/tests/login.spec.js
@@ -1,1 +1,21 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test('Valid login @sanity', async ({ page }) => {
+  const login = new LoginPage(page);
+
+  await login.goto();
+  await login.login('tomsmith', 'SuperSecretPassword!');
+
+  await expect(login.message).toContainText('You logged into a secure area!');
+});
+
+test('Invalid login @regression', async ({ page }) => {
+  const login = new LoginPage(page);
+
+  await login.goto();
+  await login.login('wrong', 'wrong');
+
+  await expect(login.message).toContainText('Your username is invalid!');
+});
 
